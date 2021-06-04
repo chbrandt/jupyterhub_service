e0bebff61029fda25c043ab4227edcf8b4ae9120
Carlos H Brandt <c.brandt@jacobs-university.de>
2021-06-04 14:57:36 +0400

    Init jupyterhub+docker+gitlab+front-form

---
+++ jupyterhub_config.py
@@ -0,0 +1,44 @@
+c = get_config()  # noqa
+
+# The docker instances need access to the Hub, so the default loopback port doesn't work:
+from jupyter_client.localinterfaces import public_ips
+c.JupyterHub.hub_ip = public_ips()[0]
+
+# Authenticator
+from oauthenticator.gitlab import GitLabOAuthenticator
+c.JupyterHub.authenticator_class = GitLabOAuthenticator
+
+from dockerspawner import DockerSpawner
+class CustomDockerSpawner(DockerSpawner):
+    def options_from_form(self, formdata):
+        options = {}
+        image_form_list = formdata.get("image", [])
+        if image_form_list and image_form_list[0]:
+            options["image"] = image_form_list[0].strip()
+            self.log.info(f"User selected image: {options['image']}")
+        return options
+
+    def load_user_options(self, options):
+        image = options.get("image")
+        if image:
+            self.log.info(f"Loading image {image}")
+            self.image = image
+
+c.JupyterHub.spawner_class = CustomDockerSpawner
+
+options_form_tpl = """
+<label for="image">Image</label>
+<input name="image" class="form-control" placeholder="the image to launch (default: {default_image})"></input>
+"""
+
+def get_options_form(spawner):
+    return options_form_tpl.format(default_image=spawner.image)
+
+c.DockerSpawner.options_form = get_options_form
+
+# pick a default image to use when none is specified
+c.DockerSpawner.image = "jupyter/base-notebook"
+
+# delete containers when they stop
+c.DockerSpawner.remove = True
+
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ue
+
+source env.rc
+
+CONFIG='jupyterhub_config.py'
+
+#sudo env "PATH=$PATH" jupyterhub --debug -f "$CONFIG" --port=8080
+sudo env PATH=$PATH \
+     OAUTH_CALLBACK_URL=$OAUTH_CALLBACK_URL \
+     OAUTH_CLIENT_ID=$OAUTH_CLIENT_ID \
+     OAUTH_CLIENT_SECRET=$OAUTH_CLIENT_SECRET \
+     GITLAB_HOST=$GITLAB_HOST \
+     jupyterhub --debug -f "$CONFIG" --port=8080
+
