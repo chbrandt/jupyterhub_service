4f93d0f94ce7bec0948a058f34a5e97a3230ca09
Carlos H Brandt <c.brandt@jacobs-university.de>
2021-11-22 06:15:30 +0100

    tests and minor tunings

--- jupyterhub_gmap/jupyterhub_config.py
+++ jupyterhub_gmap/jupyterhub_config.py
@@ -26,48 +26,59 @@ c.DockerSpawner.remove = True
 # c.SingleUserNotebookApp.default_url = "/lab"
 c.Spawner.args = ['--NotebookApp.default_url=/lab']
 
-# Explicitly set notebook directory because we'll be mounting a host volume to
-# it.  Most jupyter/docker-stacks *-notebook images run the Notebook server as
-# user `jovyan`, and set the notebook directory to `/home/jovyan/work`.
-# We follow the same convention.
-# notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
-notebook_dir = '/home/jovyan/work'
-c.DockerSpawner.notebook_dir = notebook_dir
+# # Explicitly set notebook directory because we'll be mounting a host volume to
+# # it.  Most jupyter/docker-stacks *-notebook images run the Notebook server as
+# # user `jovyan`, and set the notebook directory to `/home/jovyan/work`.
+# # We follow the same convention.
+# # notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+# notebook_dir = '/home/jovyan/work'
+# c.DockerSpawner.notebook_dir = notebook_dir
 #
 # # Mount the real user's Docker volume on the host to the notebook user's
 # # notebook directory in the container
 # c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
 
-ISISDATA = os.environ['ISISDATA']
+HOST_ISISDATA_PATH = os.environ['ISISDATA']
+ISISDATA_PATH = '/isis/data'
 # c.DockerSpawner.volumes = { ISISDATA : '/opt/conda/envs/isis/data' }
-c.DockerSpawner.volumes = { ISISDATA : '/isis/data' }
+# c.DockerSpawner.volumes = { ISISDATA : '/isis/data' }
 
 # Memory limit
 # c.Spawner.mem_limit = '3G'
 
+# From: https://discourse.jupyter.org/t/dockerspawner-and-volumes-from-host/7008/6
+#
+# Spawn a new docker for each user
 
-# # From: https://discourse.jupyter.org/t/dockerspawner-and-volumes-from-host/7008/6
-# #
-# # Spawn a new docker for each user
-# NOTEBOOK_DIR = '/home/jovyan/work'
-# HOST_HOME_PATH = "/tmp"
-# HOST_USER_PATH = HOST_HOME_PATH + "/{username}"
-# HOST_NOTEBOOK_PATH = f"{HOST_USER_PATH}/work"
-#
-# # c.JupyterHub.spawner_class = "docker"
-# # c.DockerSpawner.image = os.environ["DOCKER_JUPYTER_IMAGE"]
-# # c.DockerSpawner.network_name = os.environ["DOCKER_NETWORK_NAME"]
-# c.DockerSpawner.notebook_dir = NOTEBOOK_DIR
-# c.DockerSpawner.remove = True
-# c.DockerSpawner.extra_create_kwargs = {'user': 'root'}
-# c.DockerSpawner.environment = {
-#     "CHOWN_HOME": "yes",
-#     "CHOWN_EXTRA": "/home/jovyan",
-#     "CHOWN_HOME_OPTS": "-R",
-#     "NB_UID": 1000,
-#     "NB_GID": 1000,
-# }
-#
-# c.DockerSpawner.volumes = {
-#     f"{HOST_NOTEBOOK_PATH}": NOTEBOOK_DIR
-# }
+NOTEBOOK_DIR = '/home/jovyan/work'
+
+HOST_HOME_PATH = "/tmp"
+HOST_USER_PATH = HOST_HOME_PATH + "/{username}"
+HOST_NOTEBOOK_PATH = f"{HOST_USER_PATH}/work"
+
+# > HOST_NOTEBOOK_PATH should exist already.
+
+c.JupyterHub.spawner_class = "docker"
+c.DockerSpawner.image = "jupyter:isis"
+c.DockerSpawner.remove = True
+# c.DockerSpawner.network_name = os.environ["DOCKER_NETWORK_NAME"]
+
+c.DockerSpawner.notebook_dir = NOTEBOOK_DIR
+
+c.DockerSpawner.extra_create_kwargs = {'user': 'root'}
+c.DockerSpawner.environment = {
+    "CHOWN_HOME": "yes",
+    "CHOWN_EXTRA": "/home/jovyan",
+    "CHOWN_HOME_OPTS": "-R",
+    "NB_UID": 1000,
+    "NB_GID": 100,
+}
+
+c.DockerSpawner.volumes = {
+    f"{HOST_NOTEBOOK_PATH}": NOTEBOOK_DIR,
+    f"{HOST_ISISDATA_PATH}": ISISDATA_PATH
+}
+
+# Use Lab as the default interface
+# c.SingleUserNotebookApp.default_url = "/lab"
+c.Spawner.args = ['--NotebookApp.default_url=/lab']
--- run.sh
+++ run.sh
@@ -9,10 +9,11 @@ set -ue
 
 CONFIG="$1"
 
-sudo env PATH=$PATH \
+#sudo env PATH=$PATH \
      # OAUTH_CALLBACK_URL=$OAUTH_CALLBACK_URL \
      # OAUTH_CLIENT_ID=$OAUTH_CLIENT_ID \
      # OAUTH_CLIENT_SECRET=$OAUTH_CLIENT_SECRET \
      # GITLAB_HOST=$GITLAB_HOST \
-     ISISDATA=$ISISDATA \
-     jupyterhub --debug -f "$CONFIG" --port=8888
+#     ISISDATA=$ISISDATA \
+jupyterhub --debug -f "$CONFIG" --port=8888
+
